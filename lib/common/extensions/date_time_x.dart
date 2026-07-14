cbab969e87d677fca646808f9023d3df98ebd754
Sandro Abashidze <123871213+Iamyouandwhatiseeisme@users.noreply.github.com>
2026-07-14 16:32:23 +0400

    feat(date_time): add toTimeString method for formatted time output

--- lib/common/extensions/date_time_x.dart
+++ lib/common/extensions/date_time_x.dart
@@ -1,4 +1,10 @@
 extension DateTimeX on DateTime {
+  String toTimeString() {
+    final h = hour.toString().padLeft(2, '0');
+    final m = minute.toString().padLeft(2, '0');
+    return '$h:$m';
+  }
+
   String toRelative() {
     if (millisecondsSinceEpoch == 0) return '';
     final diff = DateTime.now().difference(this);
